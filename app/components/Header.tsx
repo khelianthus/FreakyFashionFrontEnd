7296eed2d71bd24f6916cc0d123a867594ab4d2d
Karoline Aspegren <karolineaspegren@gmail.com>
2023-05-31 17:59:51 +0200

    Add Search Component and apply to Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,6 +7,7 @@ import {
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import Search from './Search';
 
 // Inte kopplad till "caregories" i databasen ännu. 
 
@@ -43,39 +44,7 @@ export default function Header() {
               className="h-11 mr-5"
               alt="Logo"
             /> 
-
-            <form>
-              <label
-                htmlFor="default-search"
-                className="mb-2 text-sm font-medium text-gray-900 sr-only"
-                >Search</label>
-              <div className="relative">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <svg
-                    aria-hidden="true"
-                    className="w-4 h-4 text-gray-500"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z">
-                    </path>
-                  </svg>
-                </div>
-                <input
-                  type="search"
-                  id="default-search"
-                  className="block w-80 h-9 p-4 pl-10 text-xs text-gray-900 border-0 bg-gray-100 focus:ring-white placeholder-gray-700"
-                  placeholder="Sök produkt"
-                  required
-                />
-              </div>
-            </form>
+            <Search/> 
           </div>
 
           <div className="flex items-center">
