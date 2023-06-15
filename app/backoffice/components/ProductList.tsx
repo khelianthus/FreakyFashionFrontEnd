052363459a0fde1fa9d7c209d24a7329355230a2
Karoline Aspegren <karolineaspegren@gmail.com>
2023-06-15 09:57:01 +0200

    Fix ProductList

--- app/backoffice/components/ProductList.tsx
+++ app/backoffice/components/ProductList.tsx
@@ -1,80 +1,35 @@
 import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
+import { useEffect, useState } from 'react';
 import getAllProducts from '@/app/api/getAllProducts'
 import ProductDetails from '../productdetails/page'
 import Link from 'next/link';
-
-// const people = [
-//   {
-//     name: 'Leslie Alexander',
-//     email: 'leslie.alexander@example.com',
-//     role: 'Co-Founder / CEO',
-//     imageUrl:
-//       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-//     href: '#',
-//     lastSeen: '3h ago',
-//     lastSeenDateTime: '2023-01-23T13:23Z',
-//   },
-//   {
-//     name: 'Michael Foster',
-//     email: 'michael.foster@example.com',
-//     role: 'Co-Founder / CTO',
-//     imageUrl:
-//       'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-//     href: '#',
-//     lastSeen: '3h ago',
-//     lastSeenDateTime: '2023-01-23T13:23Z',
-//   },
-//   {
-//     name: 'Dries Vincent',
-//     email: 'dries.vincent@example.com',
-//     role: 'Business Relations',
-//     imageUrl:
-//       'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-//     href: '#',
-//     lastSeen: null,
-//   },
-//   {
-//     name: 'Lindsay Walton',
-//     email: 'lindsay.walton@example.com',
-//     role: 'Front-end Developer',
-//     imageUrl:
-//       'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-//     href: '#',
-//     lastSeen: '3h ago',
-//     lastSeenDateTime: '2023-01-23T13:23Z',
-//   },
-//   {
-//     name: 'Courtney Henry',
-//     email: 'courtney.henry@example.com',
-//     role: 'Designer',
-//     imageUrl:
-//       'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-//     href: '#',
-//     lastSeen: '3h ago',
-//     lastSeenDateTime: '2023-01-23T13:23Z',
-//   },
-//   {
-//     name: 'Tom Cook',
-//     email: 'tom.cook@example.com',
-//     role: 'Director of Product',
-//     imageUrl:
-//       'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-//     href: '#',
-//     lastSeen: null,
-//   },
-// ]
-
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default async function ProductList() {
+export default function ProductList() {
 
-  const productsData: Promise<Product[]> = getAllProducts();
-  const products = (await productsData);
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const productsData: Product[] = await getAllProducts();
+        setProducts(productsData);
+      } catch (error) {
+        // Handle error
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+
+  // const productsData: Promise<Product[]> = getAllProducts();
+  // const products = (await productsData);
 
 
   // const handleProductClick = (productId: string) => {
